481657aba72ef32f8ed36c896f99b04b5cf2948b
Li Kui <90845831+likui628@users.noreply.github.com>
2022-08-25 21:44:23 +0800

    feat: player movement

--- sprites.py
+++ sprites.py
@@ -1,8 +1,32 @@
-import pygame.sprite
+import pygame as pg
+from settings import *
 
 
-class Player(pygame.sprite.Sprite):
+class Player(pg.sprite.Sprite):
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((30, 40))
+        self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.vx = 0
+        self.vy = 0
+        self.acc = 0
+
+    def update(self):
+        self.acc = 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.acc = -PLAYER_ACC
+        if keys[pg.K_RIGHT]:
+            self.acc = PLAYER_ACC
+
+        self.acc += self.vx * PLAYER_FRICTION
+        self.vx += self.acc
+        self.rect.x += int(self.vx + 0.5 * self.acc)
+        self.rect.y += self.vy
+
+        if self.rect.x > WIDTH:
+            self.rect.x = 0
+        if self.rect.right < 0:
+            self.rect.right = WIDTH
